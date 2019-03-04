f477d439da704af520c9089738e613ae1986e0eb
yunjun199321 <yunjun199321@gmail.com>
2019-03-04 17:48:49 +0800

    First JavaFX demo done

--- creating_basic_window/src/sample/Main.java
+++ creating_basic_window/src/sample/Main.java
@@ -1,23 +1,38 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
+    Button btn;
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args); // javaFX starting method
+    }
+
+    /**
+     * JavaFx will call launch -> application -> start
+     * window is stage; content of the window is scene
+     *
+     * @param primaryStage
+     */
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("First JavaFx");
+        btn = new Button(); // initialize a button object
+        btn.setText("Click me");
+
+        StackPane layout = new StackPane(); // create a layout for the scene
+        layout.getChildren().add(btn); // add button object into layout
+
+        Scene scene = new Scene(layout, 300, 250); // initialize a scene object with layout, width and height
+        primaryStage.setScene(scene); // put the scene into the stage
+        primaryStage.show(); // show the stage
+
     }
 }
